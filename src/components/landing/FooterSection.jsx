f069f297c1cffb9df7558f65115597734cce3ffb
John Kyaw <jjpyae@gmail.com>
2025-04-05 09:23:26 -0400

    add social media icons and edit navbar and footer section

--- src/components/landing/FooterSection.jsx
+++ src/components/landing/FooterSection.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import '../../index.css'
+import '../../index.css';
+import { faXTwitter, faFacebook, faInstagram, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 const FooterSection = () => {
   return (
     <div id="footer">
       <div>Logo here</div>
-      <ul>
+      <ul id="footer-center-links">
         <li>About</li>
         <li>FAQs</li>
         <li>Contact Us</li>
         <li>Privacy Policy</li>
       </ul>
-      <div>Social Media icons</div>
+      <div id="social-media-icons">
+        <FontAwesomeIcon icon={faFacebook} size="lg"/>
+        <FontAwesomeIcon icon={faInstagram} size="lg"/>
+        <FontAwesomeIcon icon={faXTwitter} size="lg"/>
+        <FontAwesomeIcon icon={faLinkedin} size="lg"/>
+        <FontAwesomeIcon icon={faYoutube} size="lg"/>
+      </div>
     </div>
   )
 }
