ea922fab11c0770f69334a20acafedadc439df07
Joel <fitness.joeld@gmail.com>
2025-04-14 18:31:00 -0400

    Review form added

---
+++ src/components/ReviewForm.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+function ReviewForm({ revieweeId, setReviews }) {
+    const [rating, setRating] = useState(null);
+    const [comment, setComment] = useState('');
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newReview = {
+            reviewerId: 1, // Hardcoded for now, replace with logged-in user's ID
+            reviewerName: "Your Name", // Replace with logged-in user's name
+            revieweeId,
+            revieweeName: "Freelancer", // Replace with the freelancer's name
+            rating,
+            comment,
+        };
+
+        fetch('Fake URL', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newReview)
+        })
+            .then((respnse) => respnse.json())
+            .then((addedReview) => {
+                setReviews((PrevReviews) => [addedReview, ...PrevReviews])
+                setRating(null)
+                setComment('')
+            })
+    };
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Rating:
+                    <select value={rating} onChange={(e) => (setRating(e.target.value))}>
+                        {[1, 2, 3, 4, 5].map((n) => (
+                            <option key={n} value={n}>
+                                {n}
+                            </option>
+                        ))}
+                    </select>
+                </label>
+
+                    <br />
+                <textarea 
+                    value={comment}
+                    placeholder="Leave a Review"
+                    onChange={() => setComment(e.target.value)}
+                />
+                <button type='submit'>Submit Review</button>
+            </form>
+        </>
+    )
+}
+
+export default ReviewForm
