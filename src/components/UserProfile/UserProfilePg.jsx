b6e98c6f4fad30fca941760d882357f56d7d190a
hzeng33 <hzeng9310@gmail.com>
2025-04-29 15:50:26 -0400

    Fixed the edit profile functionality.

--- src/components/UserProfile/UserProfilePg.jsx
+++ src/components/UserProfile/UserProfilePg.jsx
@@ -10,11 +10,29 @@ const Profile = () => {
   const [profile, setProfile] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
+  const [currentUserId, setCurrentUserId] = useState(null); // Get user Id from backend response instead
 
-  const loggedInUserId = localStorage.getItem("userId");
+  // const loggedInUserId = localStorage.getItem("userId");
+
+  //To fetch current user.
+  const fetchCurrentUser = async () => {
+    try {
+      const { data } = await axios.get(
+        "http://localhost:8000/api/v1/auth/current-user",
+        { withCredentials: true }
+      );
+      setCurrentUserId(data.userId);
+    } catch (error) {
+      console.log("Error fetch current user,", error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchCurrentUser();
+  }, []);
 
   // Only check isOwner if profile exists
-  const isOwner = profile && profile.createdBy === loggedInUserId;
+  const isOwner = profile && profile.createdBy === currentUserId;
 
   useEffect(() => {
     if (!id) {
