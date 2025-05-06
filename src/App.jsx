bc1d0df398e70237223d91efe387b00dc57a17e2
hzeng33 <hzeng9310@gmail.com>
2025-05-05 20:30:54 -0400

    Created the form for creating job.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,29 @@
 import Profile from "./components/UserProfile/UserProfilePg";
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import PageWrapper from './components/PageWrapper';
-import LoginForm from './components/LoginForm';
-import RegisterForm from './components/RegisterForm';
+import PageWrapper from "./components/PageWrapper";
+import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
 
-import './App.css';
+import "./App.css";
 
-import { getAllData } from './util/index';
-import Navbar from './components/landing/Navbar.jsx';
-import HeroSection from './components/landing/HeroSection.jsx';
-import HowSection from './components/landing/HowSection.jsx';
-import TestimonialSection from './components/landing/TestimonialSection.jsx';
-import SignUpSection from  './components/landing/SignUpSection.jsx';
-import FooterSection from  './components/landing/FooterSection.jsx';
-import JobSearch from './components/JobSearch.jsx';
-import './App.css';
+import { getAllData } from "./util/index";
+import Navbar from "./components/landing/Navbar.jsx";
+import HeroSection from "./components/landing/HeroSection.jsx";
+import HowSection from "./components/landing/HowSection.jsx";
+import TestimonialSection from "./components/landing/TestimonialSection.jsx";
+import SignUpSection from "./components/landing/SignUpSection.jsx";
+import FooterSection from "./components/landing/FooterSection.jsx";
+import JobSearch from "./components/JobSearch.jsx";
+import "./App.css";
 import UserInfoForm from "./components/UserInfoForm.jsx";
-
+import JobPost from "./components/JobPost.jsx";
 
 const URL = "http://localhost:8000/api/v1/";
 
 function App() {
-
-
-  const [message, setMessage] = useState(''); 
-
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     (async () => {
@@ -40,31 +37,32 @@ function App() {
   }, []);
 
   return (
-
     <BrowserRouter>
-    <PageWrapper>
-      <Routes>
-        <Route path='/login' element={<LoginForm />} />
-        <Route path='/register' element={<RegisterForm />} />
-        <Route path="/jobs" element={<JobSearch />} />
-        <Route path="/" element={
-          <main>
-            <HeroSection />
-            <HowSection />
-            <TestimonialSection />
-            <SignUpSection />
-          </main>
-        } />
-           <Route path="/login" element={<LoginForm />} />
+      <PageWrapper>
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/jobs" element={<JobSearch />} />
+          <Route path="/create-job" element={<JobPost />} />
+          <Route
+            path="/"
+            element={
+              <main>
+                <HeroSection />
+                <HowSection />
+                <TestimonialSection />
+                <SignUpSection />
+              </main>
+            }
+          />
+          <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/profile/:id" element={<Profile />} />
           <Route path="/create-profile" element={<UserInfoForm />} />
           <Route path="/edit-profile/:id" element={<UserInfoForm />} />
-
-      </Routes>
+        </Routes>
       </PageWrapper>
     </BrowserRouter>
-
   );
 }
-export default App
+export default App;
--- src/components/landing/HeroSection.jsx
+++ src/components/landing/HeroSection.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import image from "../../images/landing1.jpg";
 import { Link } from "react-router-dom";
 
@@ -6,18 +6,23 @@ const HeroSection = () => {
   return (
     <div className="each-section" id="hero-section">
       <div>
-      <h1>Your next job is just around the corner</h1>
-      <p>Offer your skills or hire helping hands, right here in your neighborhood</p>
-      <div className="button-row">
-        <Link to="/jobs">
-          <button className="black-button">Find jobs</button>
-        </Link>
-        <button className="white-button">Post a job</button>
-      </div>        
+        <h1>Your next job is just around the corner</h1>
+        <p>
+          Offer your skills or hire helping hands, right here in your
+          neighborhood
+        </p>
+        <div className="button-row">
+          <Link to="/jobs">
+            <button className="black-button">Find jobs</button>
+          </Link>
+          <Link to="/create-job">
+            <button className="white-button">Post a job</button>
+          </Link>
+        </div>
       </div>
-      <img src={image} alt="computer" width="1200"/>
+      <img src={image} alt="computer" width="1200" />
     </div>
-  )
-}
+  );
+};
 
 export default HeroSection;
