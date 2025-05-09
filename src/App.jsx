f89394f1e7eb8a300b06055d33ce5f7a04f0571c
Joel <fitness.joeld@gmail.com>
2025-05-09 17:26:13 -0400

    Route added

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ import Navbar from './components/landing/Navbar.jsx';
 import HeroSection from './components/landing/HeroSection.jsx';
 import HowSection from './components/landing/HowSection.jsx';
 import TestimonialSection from './components/landing/TestimonialSection.jsx';
-import SignUpSection from  './components/landing/SignUpSection.jsx';
-import FooterSection from  './components/landing/FooterSection.jsx';
+import SignUpSection from './components/landing/SignUpSection.jsx';
+import FooterSection from './components/landing/FooterSection.jsx';
 import JobSearch from './components/JobSearch.jsx';
 import UserInfoForm from "./components/UserProfile/UserInfoForm.jsx";
 import JobPost from "./components/JobPost.jsx";
@@ -37,20 +37,20 @@ function App() {
 
   return (
     <BrowserRouter>
-    <PageWrapper>
-      <Routes>
-        <Route path='/login' element={<LoginForm />} />
-        <Route path='/register' element={<RegisterForm />} />
-        <Route path="/jobs" element={<JobSearch />} />
-        <Route path="/create-job" element={<JobPost />} />
-        <Route path="/" element={
-          <main>
-            <HeroSection />
-            <HowSection />
-            <TestimonialSection />
-            <SignUpSection />
-          </main>
-        } />
+      <PageWrapper>
+        <Routes>
+          <Route path='/login' element={<LoginForm />} />
+          <Route path='/register' element={<RegisterForm />} />
+          <Route path="/jobs" element={<JobSearch />} />
+          <Route path="/create-job" element={<JobPost />} />
+          <Route path="/" element={
+            <main>
+              <HeroSection />
+              <HowSection />
+              <TestimonialSection />
+              <SignUpSection />
+            </main>
+          } />
 
           <Route path="/profile/:id" element={<Profile />} />
           <Route path="/profile/:id/reviews" element={<ReviewPage />} />
