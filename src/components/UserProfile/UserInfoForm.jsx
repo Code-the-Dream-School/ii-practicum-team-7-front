8228b3565ca3913d9b4460428586e6ae6c9a3651
John Kyaw <jjpyae@gmail.com>
2025-05-11 17:49:45 -0400

    change navigate url at line 120 in UserInfoForm.jsx

--- src/components/UserProfile/UserInfoForm.jsx
+++ src/components/UserProfile/UserInfoForm.jsx
@@ -74,7 +74,7 @@ function UserInfoForm() {
         phone: data.profile.phone || "",
       });
 
-      navigate(`/profile/${data.createdBy}`); // Redirect to profile page
+      navigate(`/profile/${data.profile.createdBy}`); // Redirect to profile page
     } catch (error) {
       console.error("Error creating profile:", error.message);
     } finally {
